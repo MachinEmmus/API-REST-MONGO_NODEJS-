ff6880507c8d1a3e0f810ddf6b91835fa680f71a
MachinEmmus <emonsalvep38@gmail.com>
2020-09-23 15:37:58 -0500

    Create and endpoint and get method

--- index.js
+++ index.js
@@ -7,9 +7,13 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+app.get('/hello/:name', (req, res) => {
+    res.send({ message: `Hello ${req.params.name}!`})
+})
+
 app.listen(port, () => {
-    console.log('API REST corriendo en http://localhost:3000')
+    console.log(`API REST corriendo en http://localhost:${port}`)
 })
