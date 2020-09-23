d301ade25bf9f3d6f0155509b3c6bd3ba6d84ddb
MachinEmmus <emonsalvep38@gmail.com>
2020-09-23 15:59:57 -0500

    Postman and Api Rest wiht http methods

--- index.js
+++ index.js
@@ -10,8 +10,30 @@ const port = process.env.PORT || 3000
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.get('/hello/:name', (req, res) => {
+// Example route
+/*app.get('/hello/:name', (req, res) => {
     res.send({ message: `Hello ${req.params.name}!`})
+})*/
+
+app.get('/api/product', (req, res) => {
+    res.status(200).send({products: []})
+})
+
+app.get('/api/product/:productId', (req, res) => {
+    
+})
+
+app.post('/api/product', (req, res) => {
+    console.log(req.body)
+    res.status(200).send({message: "The product has been revived"})
+})
+
+app.put('/api/product/:productId', (req, res) => {
+    
+})
+
+app.delete('/api/product/:productId', (req, res) => {
+    
 })
 
 app.listen(port, () => {
